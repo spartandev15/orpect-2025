a34559112ed72e0b4b8c90a41c697067bc10faed
ratinderpreet <ratinderpreet@spartanbots.com>
2026-06-16 17:28:14 +0530

    Solve Bugs

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -14,6 +14,18 @@ import { logout } from "../../api/logout";
 import { IMAGE_BASE_URL_WITH_SLASH } from "../../api/baseUrl";
 
 // Routes configuration array for company sidebar
+
+
+const Layout = ({ children }) => {
+  const [search, setSearch] = useState("");
+  const [openSubmenus, setOpenSubmenus] = useState({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const searchValue = useSelector((state) => state?.dashboardData?.searchValue);
+  const user = getFromLocalStorage("user");
+  console.log(user?.is_company_user)
+  const isUser = user?.is_company_user === "user" ? false : true
 const sidebarRoutes = [
   {
     id: "submenu1",
@@ -26,7 +38,7 @@ const sidebarRoutes = [
         label: "Current Employees",
         icon: "fa fa-users",
         useSvg: true,
-        svgPath: "M211.2 96a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zM32 256c0 17.7 14.3 32 32 32h85.6c10.1-39.4 38.6-71.5 75.8-86.6c-9.7-6-21.2-9.4-33.4-9.4H96c-35.3 0-64 28.7-64 64zm461.6 32H576c17.7 0 32-14.3 32-32c0-35.3-28.7-64-64-64H448c-11.7 0-22.7 3.1-32.1 8.6c38.1 14.8 67.4 47.3 77.7 87.4zM391.2 226.4c-6.9-1.6-14.2-2.4-21.6-2.4h-96c-8.5 0-16.7 1.1-24.5 3.1c-30.8 8.1-55.6 31.1-66.1 60.9c-3.5 10-5.5 20.8-5.5 32c0 17.7 14.3 32 32 32h224c17.7 0 32-14.3 32-32c0-11.2-1.9-22-5.5-32c-10.8-30.7-36.8-54.2-68.9-61.6zM563.2 96a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zM321.6 192a80 80 0 1 0 0-160 80 80 0 1 0 0 160zM32 416c-17.7 0-32 14.3-32 32s14.3 32 32 32H608c17.7 0 32-14.3 32-32s-14.3-32-32-32H32z",
+        svgPath: "M211.2 96a64 64 0 1 0 -128 0...",
       },
       {
         path: "/ex-employee",
@@ -36,34 +48,36 @@ const sidebarRoutes = [
       },
     ],
   },
-   {
+
+  // Show only when is_company_user is false
+  isUser && {
     id: "submenu2",
     type: "submenu",
     label: "User",
     icon: "fas fa-users",
     submenu: [
-   
       {
         path: "/add-user-hr",
-        label: "Add user",
+        label: "Add User",
         icon: "fa fa-plus-circle",
         className: "logouticon curentemploye2",
       },
-        {
+      {
         path: "/list-user-hr",
-        label: "List user",
+        label: "List User",
         icon: "fa fa-users",
         className: "logouticon curentemploye2",
       },
     ],
   },
+
   {
     type: "link",
     path: "/add-position",
     label: "Add Position",
     icon: "fa fa-plus-circle",
-    useSvg: true,
-    svgPath: "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z",
+    // useSvg: true,
+    // svgPath: "M256 512A256 256 0 1 0 256 0...",
   },
   {
     type: "link",
@@ -105,30 +119,21 @@ const sidebarRoutes = [
     type: "link",
     path: "/profile",
     label: "Profile",
-    icon: "fa fa-user",
+    icon: "fa fa-circle-user",
   },
-   {
+  {
     type: "link",
     path: "/log-list",
     label: "Log List",
     icon: "fa fa-user",
   },
   {
-        type: "link",
-        path: "/hrms",
-        label: "HRMS",
-        icon: "fa fa-user",
-      },
-];
-
-const Layout = ({ children }) => {
-  const [search, setSearch] = useState("");
-  const [openSubmenus, setOpenSubmenus] = useState({});
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const searchValue = useSelector((state) => state?.dashboardData?.searchValue);
-  const user = getFromLocalStorage("user");
+    type: "link",
+    path: "/hrms",
+    label: "HRMS",
+    icon: "fa fa-user",
+  },
+].filter(Boolean);
   const profileImage = getFromLocalStorage("profileImage");
 const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   const logoutUser = () => {
